d7e0ec0c3bd8d7bfde89b284e4a7c12a6c396981
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-05-23 16:48:49 +0200

    fix(pms): assert price in value object

--- src/apps/pms/sam-app/lambda/create-product/handler.ts
+++ src/apps/pms/sam-app/lambda/create-product/handler.ts
@@ -9,6 +9,7 @@ import { Logger } from "@shared/domain/Logger";
 import ProductCreatorHandler from "@pms-contexts/products/app/create/ProductCreatorHandler";
 import CreateProductCommand from "@pms-contexts/products/app/create/CreateProductCommand";
 import {ProductPrimitives} from "@pms-contexts/products/domain/ProductPrimitives";
+import InvalidPriceError from "@pms-contexts/products/domain/InvalidPriceError";
 
 const container = new ContainerBuilder();
 sharedRegister(container);
@@ -26,12 +27,20 @@ const logger: Logger = container.get(
 export const handler = async (event: EventBridgeEvent<string, ProductPrimitives>, _context: Context) => {
     logger.info(`Event Bridge event received{ id: ${event.id} and detail: ${event.detail}}`);
     // eslint-disable-next-line one-var
-    const id = event.detail.id,
-        commerceId = event.detail.commerceId,
-        name = event.detail.name,
-        price = event.detail.price,
-        description = event.detail.description,
-        createProductCommand = new CreateProductCommand(id, commerceId, name, price, description),
-        product = await handlerCreator.handle(createProductCommand);
-    logger.info(`Product created: ${JSON.stringify(product)}`);
+    try {
+        const id = event.detail.id,
+            commerceId = event.detail.commerceId,
+            name = event.detail.name,
+            price = event.detail.price,
+            description = event.detail.description,
+            createProductCommand = new CreateProductCommand(id, commerceId, name, price, description),
+            product = await handlerCreator.handle(createProductCommand);
+        logger.info(`Product created: ${JSON.stringify(product)}`);
+    } catch (error) {
+        if (error instanceof InvalidPriceError) {
+            logger.error(`Invalid price: ${error.message}`);
+        } else {
+            logger.error(`Not known error`);
+        }
+    }
 };
