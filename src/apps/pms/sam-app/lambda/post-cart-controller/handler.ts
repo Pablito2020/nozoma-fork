23d0f671484e5b555ad940d6131177d89c5b9812
FerranAD <farandomingo@gmail.com>
2022-05-26 16:07:11 +0200

    feat(cart): add sqs

--- src/apps/pms/sam-app/lambda/post-cart-controller/handler.ts
+++ src/apps/pms/sam-app/lambda/post-cart-controller/handler.ts
@@ -12,6 +12,8 @@ import {Logger} from "@shared/domain/Logger";
 import CreateCartCommand from "@pms-contexts/carts/app/create/CreateCartCommand";
 import InvalidArgumentError from "@shared/domain/InvalidArgumentError";
 import AlreadyExists from "@shared/domain/AlreadyExists";
+import {AWSError} from "aws-sdk";
+import {SendMessageResult} from "aws-sdk/clients/sqs";
 
 const container = new ContainerBuilder();
 sharedRegister(container);
@@ -29,6 +31,41 @@ const logger: Logger = container.get(
     ) => {
         logger.info(`REQUEST PATH: ${event.path}`);
         logger.info(`REQUEST BODY: ${event.body}`);
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        var AWS = require('aws-sdk');
+        AWS.config.update({region: 'REGION'});
+        const sqs = new AWS.SQS({apiVersion: '2012-11-05'}),
+            params = {
+            // Remove DelaySeconds parameter and value for FIFO queues
+                DelaySeconds: 10,
+                MessageAttributes: {
+                    "Title": {
+                        DataType: "String",
+                        StringValue: "The Whistler"
+                    },
+                    "Author": {
+                        DataType: "String",
+                        StringValue: "John Grisham"
+                    },
+                    "WeeksOn": {
+                        DataType: "Number",
+                        StringValue: "6"
+                    }
+                },
+                MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+                // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+                // MessageGroupId: "Group1",  // Required for FIFO queues
+                QueueUrl: "https://sqs.eu-west-1.amazonaws.com/327832251428/backoffice-joel-patrick-event-cart-updated-sqs"
+            };
+
+        sqs.sendMessage(params, function(err:AWSError, data: SendMessageResult) {
+            if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data.MessageId);
+            }
+        });
+
         try {
             const {
                     cartId
